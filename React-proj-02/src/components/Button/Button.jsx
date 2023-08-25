0dede476f5e98aba3210ecd9d2e46d42d8532d6e
iamaman526 <iamaman526@gmail.com>
2023-08-25 19:27:23 +0530

    working on contact us form

---
+++ React-proj-02/src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import styles from "./Button.module.css";
+
+// eslint-disable-next-line react/prop-types
+const Button = ({ isOutline, icon, text }) => {
+  return (
+    <button className={isOutline ? styles.outline_btn : styles.primary_btn}>
+      {icon}
+      {text}
+    </button>
+  );
+};
+
+export default Button;
--- React-proj-02/src/components/ContactForm/ContactForm.jsx
+++ React-proj-02/src/components/ContactForm/ContactForm.jsx
@@ -1,18 +1,30 @@
-import styles from "./ContactForm.module.css"
+import Button from "../Button/Button";
+import styles from "./ContactForm.module.css";
+import { MdMessage } from "react-icons/md";
+import { MdLocalPhone } from "react-icons/md";
+import { MdOutlineMail } from "react-icons/md";
 
 const ContactForm = () => {
   return (
     <section className={styles.container}>
-   <div className={styles.contact_form}>
+      <div className={styles.contact_form}>
+        <div className={styles.top_btn}>
+          <Button
+            text="VIA SUPPORT CHAT"
+            icon={<MdMessage fontSize="24px" />}
+          />
+          <Button text="VIA CALL" icon={<MdLocalPhone fontSize="24px" />} />
+        </div>
 
-   </div>
-   <div className={styles.contactImage}>
-
-   </div>
-
-
+        <Button
+          isOutline={true}
+          text="VIA EMAIL FORM"
+          icon={<MdOutlineMail fontSize="24px" />}
+        />
+      </div>
+      <div className={styles.contact_image}></div>
     </section>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
