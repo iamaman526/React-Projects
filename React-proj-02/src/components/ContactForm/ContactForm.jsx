d369a30f242144727b5ae01a337b2b3a6d926481
iamaman526 <iamaman526@gmail.com>
2023-08-25 23:01:25 +0530

    working on contact us form

--- React-proj-02/src/components/ContactForm/ContactForm.jsx
+++ React-proj-02/src/components/ContactForm/ContactForm.jsx
@@ -21,8 +21,30 @@ const ContactForm = () => {
           text="VIA EMAIL FORM"
           icon={<MdOutlineMail fontSize="24px" />}
         />
+
+        <form>
+           <div className={styles.form_control} > <label htmlFor="name">Name</label>
+          <input type="text" name="name" /></div>
+
+          <div className={styles.form_control} > <label htmlFor="name">Email</label>
+          <input type="email" name="email" /></div>
+
+          <div className={styles.form_control} > <label htmlFor="name">Text</label>
+          <textarea name="text"   rows="8"/></div>
+          <div style={{
+            display: "flex",
+            justifyContent: "end"
+          }}>  
+             <Button
+          text="SUBMIT BUTTON"
+        /></div>
+       
+          
+        </form> 
       </div>
-      <div className={styles.contact_image}></div>
+      <div className={styles.contact_image}>
+        <img src="/images/service.svg" alt="contact image" />
+      </div>
     </section>
   );
 };
